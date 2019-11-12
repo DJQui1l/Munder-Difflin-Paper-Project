99689c147feb801f1ecb5930c31de2c9d5bb3169
Darian Jarvis <djarvis@nyc.yearup.org>
2019-11-12 13:12:15 -0500

    before presentation, added a scroll to top button.

--- main.js
+++ main.js
@@ -11,3 +11,32 @@ function toggleText (btn, id) {
     btn.innerHTML = "Show Less";
   }
 }
+//////////////////////////////////
+
+
+
+mybutton = document.getElementById('img-btn');
+
+window.onscroll = function() { scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop =0;
+}
+
+
+// imgButton = document.getElementById('img-btn');
+//
+// window.onscroll = function () {scrollFunction()};
+//
+// function scrollFunction() {
+//   if
+// }
